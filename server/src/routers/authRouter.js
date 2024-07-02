3cafcd8fbbce2253e156c4fb4d2e6b27699a3687
habibsha <habibshahalahi7424@gmail.com>
2024-07-02 21:24:23 +0600

    create authenticate middleware

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -2,10 +2,11 @@ const {
   handleProcessRegister,
   handleLogin,
 } = require("../controllers/authController");
+const authenticate = require("../middleware/authenticate");
 
 const authRouter = require("express").Router();
 
 authRouter.post("/process-register", handleProcessRegister);
-authRouter.post("/login", handleLogin);
+authRouter.post("/login", authenticate, handleLogin);
 
 module.exports = authRouter;
